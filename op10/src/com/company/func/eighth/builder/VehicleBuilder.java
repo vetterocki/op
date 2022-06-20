17a8b9dc78792356bcf28b5943a7b1e7882d2764
Vitalii Butriy <92949251+vetterocki@users.noreply.github.com>
2022-06-20 22:46:33 +0300

    Delete op10 directory

--- op10/src/com/company/func/eighth/builder/VehicleBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.company.func.eighth.builder;
-
-public interface VehicleBuilder<T> {
-    T price(int price);
-    T maxSpeed(double maximumSpeed);
-    T manuName(String manufactureName);
-
-
-}
